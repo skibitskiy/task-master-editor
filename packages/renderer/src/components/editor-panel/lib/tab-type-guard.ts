d6b233e9af60f2ba1f685edb4d11d04cc129da68
moskibitskiy <moskibitskiy@avito.ru>
2025-09-12 00:29:56 +0500

    refactor(renderer): Удалил лишние файлы

--- packages/renderer/src/components/editor-panel/lib/tab-type-guard.ts
+++
@@ -1,5 +0,0 @@
-import { TaskFieldTab } from './types';
-
-export const tabTypeGuard = (tab: string): tab is TaskFieldTab => {
-  return ['title', 'description', 'details', 'dependencies', 'testStrategy'].includes(tab);
-};
--- packages/renderer/src/components/editor-panel/lib/types.ts
+++
@@ -1,3 +0,0 @@
-import type { Task } from '@app/shared';
-
-export type TaskFieldTab = Extract<keyof Task, 'title' | 'description' | 'details' | 'dependencies' | 'testStrategy'>;
